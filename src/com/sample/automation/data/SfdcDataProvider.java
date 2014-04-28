6a82e2ceaad02f3a3d8cc9bd69efc507361014b1
PoonamYadav73 <poonam.yadav@capgemini.com>
2014-04-28 17:04:16 +0530

    save path

--- src/com/sample/automation/data/SfdcDataProvider.java
+++ src/com/sample/automation/data/SfdcDataProvider.java
@@ -10,7 +10,7 @@ public class SfdcDataProvider {
 	public static Object[][] extract() throws Exception{
       
 		TableData l=new TableData();
-		Object[][] retObjArr=l.getTableArray("D:\\Allegis SFDC\\Allegis\\SFDC_TEST_Sandbox-master\\resources\\data\\testdata.xls",
+		Object[][] retObjArr=l.getTableArray("C:\\Users\\jnehal\\git\SFDC_TEST_REP\\SampleTestAutomationProject\\resources\\data\\testdata.xls",
               "userlogin", "Credentials");
       return(retObjArr);
 	}
@@ -19,7 +19,7 @@ public class SfdcDataProvider {
 	public static Object[][] extract1() throws Exception{
       
 		TableData l1=new TableData();
-		Object[][] retObjArr1=l1.getTableArray("D:\\Allegis SFDC\\Allegis\\SFDC_TEST_Sandbox-master\\resources\\data\\testdata.xls",
+		Object[][] retObjArr1=l1.getTableArray("C:\\Users\\jnehal\\git\\SFDC_TEST_REP\\SampleTestAutomationProject\\resources\\data\\testdata.xls",
               "userlogin", "Report");
       return(retObjArr1);
 	}
